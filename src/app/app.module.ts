03f2a3ac4202af2852ac3f9766ab67ff7e970042
ailequal <37016865+ailequal@users.noreply.github.com>
2022-06-16 12:18:29 +0200

    added angular material package

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {AppRoutingModule} from './app-routing.module';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {CoreModule} from './core/core.module';
 
 import {AppComponent} from './app.component';
@@ -12,6 +13,7 @@ import {AppComponent} from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
     CoreModule
   ],
   providers: [],
