13516754ffb72f7c4ecdd342bd9de47ee6279f8c
ailequal <37016865+ailequal@users.noreply.github.com>
2022-06-23 18:03:06 +0200

    working on auth service

---
+++ src/app/api/auth.service.ts
@@ -0,0 +1,97 @@
+import firebase from "firebase/compat/app";
+import {AngularFireAuth} from "@angular/fire/compat/auth";
+import {AngularFirestore, AngularFirestoreDocument} from "@angular/fire/compat/firestore";
+import {Injectable} from '@angular/core';
+import {Observable, of, switchMap} from "rxjs";
+import {Router} from "@angular/router";
+import {User} from "../models/user";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  user$: Observable<User | undefined> | undefined = undefined;
+
+  /**
+   * The constructor function.
+   *
+   * @param _router
+   * @param _fireAuth
+   * @param _fireStore
+   */
+  constructor(
+    private _router: Router,
+    private _fireAuth: AngularFireAuth,
+    private _fireStore: AngularFirestore
+  ) {
+    // Remap the default Firebase user object with our custom data attached.
+    this.user$ = this._fireAuth.authState.pipe(
+      switchMap(user => {
+        return user ?
+          this._fireStore.doc<User>(`users/${user.uid}`).valueChanges() :
+          of(undefined);
+      })
+    );
+  }
+
+  /**
+   * Handle the login through email.
+   */
+  async emailLogin(): Promise<void> {
+    // TODO: Implement the standard login with the email.
+    return new Promise<void>(() => {
+    })
+
+    // return await this.editUser(credential.user); // The result will always be undefined.
+  }
+
+  /**
+   * Handle the login through Google.
+   */
+  async googleLogin(): Promise<void> {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    const credential = await this._fireAuth.signInWithPopup(provider);
+
+    return await this.editUser(credential.user); // The result will always be undefined.
+  }
+
+  /**
+   * Add or update the logged-in user into the users' collection.
+   *
+   * @param user
+   * @private
+   */
+  private editUser(user: firebase.User | null): Promise<void> {
+    if (!user) {
+      return new Promise<void>(() => {
+      })
+    }
+
+    // Create the local relative user document reference.
+    const userDocument: AngularFirestoreDocument<User> = this._fireStore.doc(`users/${user.uid}`);
+
+    // Gather all the user data.
+    const data: User = {
+      uid: user.uid,
+      email: user.email ? user.email : '',
+      displayName: user.displayName ? user.displayName : '',
+      photoURL: user.photoURL ? user.photoURL : '',
+      lastLogin: Date.now() // The timestamp of the last login.
+    }
+
+    // Set the user data and return the result (promise with void).
+    return userDocument.set(data, {merge: true})
+  }
+
+  /**
+   * Handle the logout.
+   */
+  async logout(): Promise<boolean> {
+    await this._fireAuth.signOut();
+
+    // TODO: Decide how to handle the routing for the auth.
+    return await this._router.navigateByUrl('/');
+  }
+
+}
